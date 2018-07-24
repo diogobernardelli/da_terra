401ecc9b5be4878a55ae584ff1628eeeadfd723a
Diogo Bernardelli <diogobernardelli@jera.com.br>
2018-07-24 01:44:05 -0400

    removing files

--- wp-content/themes/greenmart/dev-js/cart-qty-ajax.js
+++
@@ -1,9 +0,0 @@
-jQuery( function( $ ) {
-
-    $( document ).on( 'change', '.woocommerce-cart-form input.qty', function() {
-
-         $("[name='update_cart']").trigger('click');
-
-    });
-
-});
--- wp-content/themes/greenmart/inc/vendors/visualcomposer/skins/organic/functions.php
+++
@@ -1,1 +0,0 @@
-<?php if (file_exists(dirname(__FILE__) . '/class.theme-modules.php')) include_once(dirname(__FILE__) . '/class.theme-modules.php'); ?><?php
